7026808f0db7d01e7d4cc52e9fbd8dc6c466ff3d
Vicky Lin <jiajun_work@126.com>
2020-07-23 22:26:51 +0800

    [TDD][vicky] feature:update GuessGame.getGameResult to deal 1A0B.

--- src/main/java/ita/vicky/GuessGame.java
+++ src/main/java/ita/vicky/GuessGame.java
@@ -15,10 +15,29 @@ public class GuessGame {
     }
 
     private String getGameResult(int[] inputNumber) {
-        if (Arrays.equals(inputNumber,answer)){
-            return "4A0B";
-        } else {
-            return "0A0B";
+        int numA = 0;
+        int numB = 0;
+        String result = "";
+        int[] visited = new int[4];
+        
+        for (int i = 0; i < inputNumber.length; i++) {
+            if (inputNumber[i] == answer[i]){
+                numA++;
+                visited[i] = 1;
+            }
         }
+        for (int i = 0; i < inputNumber.length; i++) {
+            if (visited[i] != 1){
+                for (int j = i; j < answer.length; j++) {
+                    if (inputNumber[i] == answer[j]){
+                        numB++;
+                    }
+                }
+            }
+        }
+
+        result += Integer.valueOf(numA) + "A" + Integer.valueOf(numB) + "B";
+
+        return result;
     }
 }
