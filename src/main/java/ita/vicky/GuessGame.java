12a1e774f22c22c5468afc3fde1c95b0d6905111
Vicky Lin <jiajun_work@126.com>
2020-07-23 22:30:39 +0800

    [TDD][vicky] refactor:add GuessGame.getNumA and GuessGame.getNumB.

--- src/main/java/ita/vicky/GuessGame.java
+++ src/main/java/ita/vicky/GuessGame.java
@@ -1,7 +1,5 @@
 package ita.vicky;
 
-import java.util.Arrays;
-
 public class GuessGame {
 
     private int[] answer;
@@ -19,13 +17,16 @@ public class GuessGame {
         int numB = 0;
         String result = "";
         int[] visited = new int[4];
-        
-        for (int i = 0; i < inputNumber.length; i++) {
-            if (inputNumber[i] == answer[i]){
-                numA++;
-                visited[i] = 1;
-            }
-        }
+
+        numA = getNumA(inputNumber, numA, visited);
+        numB = getNumB(inputNumber, numB, visited);
+
+        result += String.format("%dA%dB", Integer.valueOf(numA), Integer.valueOf(numB));
+
+        return result;
+    }
+
+    private int getNumB(int[] inputNumber, int numB, int[] visited) {
         for (int i = 0; i < inputNumber.length; i++) {
             if (visited[i] != 1){
                 for (int j = i; j < answer.length; j++) {
@@ -35,9 +36,16 @@ public class GuessGame {
                 }
             }
         }
+        return numB;
+    }
 
-        result += Integer.valueOf(numA) + "A" + Integer.valueOf(numB) + "B";
-
-        return result;
+    private int getNumA(int[] inputNumber, int numA, int[] visited) {
+        for (int i = 0; i < inputNumber.length; i++) {
+            if (inputNumber[i] == answer[i]){
+                numA++;
+                visited[i] = 1;
+            }
+        }
+        return numA;
     }
 }
