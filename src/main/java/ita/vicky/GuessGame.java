365d84f36bf5d5d1878b4458c7523ef10d529cc7
Vicky Lin <jiajun_work@126.com>
2020-07-23 21:18:30 +0800

    [TDD][vicky] test:add should_return_4A0B_when_guess_given_1234.

---
+++ src/main/java/ita/vicky/GuessGame.java
@@ -0,0 +1,14 @@
+package ita.vicky;
+
+public class GuessGame {
+
+    private String answer;
+
+    public GuessGame(int[] answer) {
+        this.answer = answer;
+    }
+
+    public String play(int[] inputNumber) {
+        return null;
+    }
+}
