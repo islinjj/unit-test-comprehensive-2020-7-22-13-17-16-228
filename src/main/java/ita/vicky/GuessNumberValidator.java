85950a3cfbbdad4c8b0290437edf85ac055ac07b
Vicky Lin <jiajun_work@126.com>
2020-07-26 22:32:45 +0800

    [GuessGameTDD][vicky] refactor:update GuessNumberValidator.

--- src/main/java/ita/vicky/GuessNumberValidator.java
+++ src/main/java/ita/vicky/GuessNumberValidator.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 
 public class GuessNumberValidator {
     public boolean isValid(int[] inputNumber) {
-        return inputNumber.length == 4 && Arrays.stream(inputNumber).noneMatch(input -> Arrays.stream(inputNumber).noneMatch(i -> i == input)) && !String.valueOf(inputNumber).matches(".*^[\\d]+.*");
+        return isValidLength(inputNumber) && isNotRepeatNumber(inputNumber) && isAllNumber(inputNumber);
+    }
+
+    private boolean isValidLength(int[] inputNumber) {
+        return inputNumber.length == 4;
+    }
+
+    private boolean isAllNumber(int[] inputNumber) {
+        return !String.valueOf(inputNumber).matches(".*^[\\d]+.*");
+    }
+
+    private boolean isNotRepeatNumber(int[] inputNumber) {
+        return Arrays.stream(inputNumber).noneMatch(input -> Arrays.stream(inputNumber).noneMatch(i -> i == input));
     }
 }
