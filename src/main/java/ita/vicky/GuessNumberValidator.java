572a35475914080cd40466e947d139b3e232f8a7
Vicky Lin <jiajun_work@126.com>
2020-07-26 22:07:43 +0800

    [GuessGameTDD][vicky] feature:add check input number is repeat.

--- src/main/java/ita/vicky/GuessNumberValidator.java
+++ src/main/java/ita/vicky/GuessNumberValidator.java
@@ -1,7 +1,9 @@
 package ita.vicky;
 
+import java.util.Arrays;
+
 public class GuessNumberValidator {
     public boolean isValid(int[] inputNumber) {
-        return inputNumber.length == 4;
+        return inputNumber.length == 4 && Arrays.stream(inputNumber).noneMatch(input -> Arrays.stream(inputNumber).anyMatch(i -> i == input));
     }
 }
