81485c277f7f87753a245295dc19af6c6ef71e67
Vicky Lin <jiajun_work@126.com>
2020-07-26 16:37:59 +0800

    [GuessGameTDD][vicky] feature:deal "0A0B".

---
+++ src/main/java/ita/vicky/GenerateAnswer.java
@@ -0,0 +1,7 @@
+package ita.vicky;
+
+public class GenerateAnswer {
+    public Object generate() {
+        return null;
+    }
+}
---
+++ src/main/java/ita/vicky/GuessNumberGame.java
@@ -0,0 +1,13 @@
+package ita.vicky;
+
+public class GuessNumberGame {
+    private GenerateAnswer generateAnswer;
+
+    public GuessNumberGame(GenerateAnswer generateAnswer) {
+        this.generateAnswer = generateAnswer;
+    }
+
+    public String guess(String inputNumber) {
+        return "0A0B";
+    }
+}
