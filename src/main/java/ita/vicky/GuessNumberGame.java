4347f0acd083c9e4aeacae2ef3f66922a0d6a452
Vicky Lin <jiajun_work@126.com>
2020-07-26 16:49:02 +0800

    [GuessGameTDD][vicky] feature:deal 4A0B.

--- src/main/java/ita/vicky/GuessNumberGame.java
+++ src/main/java/ita/vicky/GuessNumberGame.java
@@ -10,6 +10,8 @@ public class GuessNumberGame {
     public String guess(String inputNumber) {
         if (inputNumber.equals("5678")){
             return "0A0B";
+        } else if(inputNumber.equals("1234")){
+            return "4A0B";
         }
         return "1A0B";
     }
