8ae139215dc975327b08444a3d812840361830b3
Vicky Lin <jiajun_work@126.com>
2020-07-26 21:39:49 +0800

    [GuessGameTDD][vicky] refactor:add GuessNumberGame.getInputNumberEqualsAnswerIgnorePosition.

--- src/main/java/ita/vicky/GuessNumberGame.java
+++ src/main/java/ita/vicky/GuessNumberGame.java
@@ -1,6 +1,5 @@
 package ita.vicky;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,14 +13,12 @@ public class GuessNumberGame {
 
     public String guess(int[] inputNumber) {
         int countOutputANum = getCountOutputANum(inputNumber);
-        if (Arrays.equals(inputNumber,new int[]{4,3,2,1})){
-            return "0A4B";
-        } else if (Arrays.equals(inputNumber,new int[]{2,4,7,8})){
-            return "0A2B";
-        } else if (Arrays.equals(inputNumber,new int[]{0,3,2,4})){
-            return "1A2B";
-        }
-        return String.format("%dA0B", countOutputANum);
+        int countOutputBNum = getInputNumberEqualsAnswerIgnorePosition(inputNumber) - countOutputANum;
+        return String.format("%dA%dB", countOutputANum,countOutputBNum);
+    }
+
+    private int getInputNumberEqualsAnswerIgnorePosition(int[] inputNumber) {
+        return (int)Arrays.stream(inputNumber).filter(input -> Arrays.stream(answer).anyMatch(ans -> ans == input)).count();
     }
 
     private int getCountOutputANum(int[] inputNumber) {
