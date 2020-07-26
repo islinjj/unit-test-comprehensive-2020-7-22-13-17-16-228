9abd9b01742f49d0ab0f535463b51cd821480436
Vicky Lin <jiajun_work@126.com>
2020-07-26 22:45:34 +0800

    [GuessGameTDD][vicky] test:add should_return_true_when_validate_given_random_answer.

---
+++ src/test/java/ita/vicky/AnswerValidatorTest.java
@@ -0,0 +1,21 @@
+package ita.vicky;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AnswerValidatorTest {
+    @Test
+    void should_return_true_when_validate_given_random_answer() {
+        //given
+        GenerateAnswer generateAnswer = new GenerateAnswer();
+        AnswerValidator answerValidator = new AnswerValidator();
+        int[] answer = generateAnswer.generate();
+
+        //when
+        boolean isAnswerValid = answerValidator.isValid(answer);
+
+        //then
+        assertTrue(isAnswerValid);
+    }
+}
