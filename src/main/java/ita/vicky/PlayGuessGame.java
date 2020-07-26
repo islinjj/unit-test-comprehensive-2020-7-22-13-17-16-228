0703808269d5cc9ceffa65c00a3ff30a767c3dda
Vicky Lin <jiajun_work@126.com>
2020-07-27 00:47:28 +0800

    [GuessGameTDD][vicky] refactor:win game.

--- src/main/java/ita/vicky/PlayGuessGame.java
+++ src/main/java/ita/vicky/PlayGuessGame.java
@@ -3,8 +3,20 @@ package ita.vicky;
 import java.util.List;
 
 public class PlayGuessGame {
+    private GenerateAnswer generateAnswer;
+    private GuessNumberGame guessNumberGame;
+
+    public PlayGuessGame() {
+        generateAnswer = new GenerateAnswer();
+        guessNumberGame = new GuessNumberGame(new int[]{1, 2, 3, 4});
+    }
+
     public String play(List<int[]> inputNumberList) {
-        System.out.print("0A4B\n4A0B\n");
+        String guessGameResult = "";
+        for (int[] inputNumber:inputNumberList){
+            guessGameResult = guessNumberGame.guess(inputNumber);
+            System.out.printf("%s\n", guessGameResult);
+        }
         return null;
     }
 }
