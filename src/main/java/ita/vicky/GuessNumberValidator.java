8a5878ec2af46b612754fa61bb9f2d4cece55abb
Vicky Lin <jiajun_work@126.com>
2020-07-26 21:59:08 +0800

    [GuessGameTDD][vicky] feature:add check inputNumber smaller than 4.

--- src/main/java/ita/vicky/GuessNumberValidator.java
+++ src/main/java/ita/vicky/GuessNumberValidator.java
@@ -2,6 +2,9 @@ package ita.vicky;
 
 public class GuessNumberValidator {
     public boolean isValid(int[] inputNumber) {
+        if (inputNumber.length < 4){
+            return false;
+        }
         return true;
     }
 }
