ff403ef7165a0aa288cef97146281b95d0b67aef
Vicky Lin <jiajun_work@126.com>
2020-07-27 22:03:18 +0800

    [GuessGameTDD][vicky] refactor: add getCountOutputBNum and format.

--- src/main/java/ita/vicky/GuessNumberGame.java
+++ src/main/java/ita/vicky/GuessNumberGame.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static ita.vicky.Number.NUMBER_LIMIT_LENGTH;
+
 public class GuessNumberGame {
     private int[] answer;
 
@@ -13,16 +15,26 @@ public class GuessNumberGame {
 
     public String guess(int[] inputNumber) {
         int countOutputANum = getCountOutputANum(inputNumber);
-        int countOutputBNum = getInputNumberEqualsAnswerIgnorePosition(inputNumber) - countOutputANum;
+        int countOutputBNum = getCountOutputBNum(inputNumber, countOutputANum);
         return String.format("%dA%dB", countOutputANum,countOutputBNum);
     }
 
+    private int getCountOutputBNum(int[] inputNumber, int countOutputANum) {
+        return getInputNumberEqualsAnswerIgnorePosition(inputNumber) - countOutputANum;
+    }
+
     private int getInputNumberEqualsAnswerIgnorePosition(int[] inputNumber) {
-        return (int)Arrays.stream(inputNumber).filter(input -> Arrays.stream(answer).anyMatch(ans -> ans == input)).count();
+        return (int)Arrays
+                .stream(inputNumber)
+                .filter(input -> Arrays.stream(answer).anyMatch(ans -> ans == input))
+                .count();
     }
 
     private int getCountOutputANum(int[] inputNumber) {
-        return IntStream.range(0,inputNumber.length).mapToObj(index -> inputNumber[index] == answer[index])
-                .filter(isInputNumberEqualsAnswer -> isInputNumberEqualsAnswer == true).collect(Collectors.toList()).size();
+        return IntStream
+                .range(0,NUMBER_LIMIT_LENGTH)
+                .mapToObj(index -> inputNumber[index] == answer[index])
+                .filter(isInputNumberEqualsAnswer -> isInputNumberEqualsAnswer == true)
+                .collect(Collectors.toList()).size();
     }
 }
