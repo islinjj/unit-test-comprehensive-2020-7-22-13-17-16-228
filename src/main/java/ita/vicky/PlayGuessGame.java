a2559dd5319e1e9834744a87332e1a85fec5b28a
Vicky Lin <jiajun_work@126.com>
2020-07-27 19:33:42 +0800

    [GuessGameTDD][vicky] feature:print wrong input when input invalid number .

--- src/main/java/ita/vicky/PlayGuessGame.java
+++ src/main/java/ita/vicky/PlayGuessGame.java
@@ -5,16 +5,22 @@ import java.util.List;
 public class PlayGuessGame {
     private GenerateAnswer generateAnswer;
     private GuessNumberGame guessNumberGame;
+    private NumberValidator numberValidator;
 
     public PlayGuessGame() {
         generateAnswer = new GenerateAnswer();
+        numberValidator = new NumberValidator();
     }
 
     public String play(int[] inputNumber,int[] answer) {
         guessNumberGame = new GuessNumberGame(answer);
-        String guessGameResult = "";
-        guessGameResult = guessNumberGame.guess(inputNumber);
-        System.out.printf("%s\n", guessGameResult);
+        if (numberValidator.isValid(inputNumber)){
+            String guessGameResult = "";
+            guessGameResult = guessNumberGame.guess(inputNumber);
+            System.out.printf("%s\n", guessGameResult);
+        } else {
+            System.out.printf("wrong input, input again\n");
+        }
         return null;
     }
 }
