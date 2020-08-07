ffe1256862cec3a271e1629e7a2b72aef9d8016b
Vicky Lin <jiajun_work@126.com>
2020-08-07 22:27:22 +0800

    [GuessGameTDD][vicky] refactor: delete unused package and param.

--- src/main/java/ita/vicky/PlayGuessGame.java
+++ src/main/java/ita/vicky/PlayGuessGame.java
@@ -1,37 +1,31 @@
 package ita.vicky;
 
-import java.util.Scanner;
-
 import static ita.vicky.normal.Message.END_GAME;
 import static ita.vicky.normal.Message.WRONG_INPUT;
-import static ita.vicky.normal.Number.*;
 
 public class PlayGuessGame {
     private GuessNumberGame guessNumberGame;
     private NumberValidator numberValidator;
     private int countPlayTimes;
+    private InputNumber inputNumber;
 
     public PlayGuessGame() {
         countPlayTimes = 6;
         numberValidator = new NumberValidator();
+        inputNumber = new InputNumber();
     }
+
 
     public void play(int[] answer) {
         guessNumberGame = new GuessNumberGame(answer);
         for (int time = 0; time < countPlayTimes; time++) {
-            int[] inputNumber = getInput();
-            System.out.print(numberValidator.isValid(inputNumber) ? keepGuess(inputNumber) : WRONG_INPUT);
+            int[] number = inputNumber.getInput();
+            System.out.print(numberValidator.isValid(number) ? keepGuess(number) : WRONG_INPUT);
+            if ("4A0B".equals(keepGuess(number))){
+                break;
+            }
         }
         endGame();
-    }
-
-    private int[] getInput() {
-        Scanner scan = new Scanner(System.in);
-        int[] inputNumber = new int[NUMBER_LIMIT_LENGTH];
-        for (int i = 0; i < inputNumber.length; i++) {
-            inputNumber[i] = scan.nextInt();
-        }
-        return inputNumber;
     }
 
     private String keepGuess(int[] inputNumber) {
